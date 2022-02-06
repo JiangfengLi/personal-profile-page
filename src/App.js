0e40e2c87210313825389b7c689bdae1e1bb4a7a
JiangfengLi <31572875+JiangfengLi@users.noreply.github.com>
2022-02-05 21:54:36 -0800

    Deleted jquery module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -12,14 +11,14 @@ import { resumeData } from "./constants/config";
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    // this.state = {
-    //   foo: 'bar',
-    //   resumeData: {}
-    // };
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     foo: 'bar',
+  //     resumeData: {}
+  //   };
 
-  }
+  // }
 
   // getResumeData(){
   //   $.ajax({
